262c2ec69cc168f3105a1fbf234d3611c4906cc3
brahianf <brahianf@unicauca.edu.co>
2019-09-29 11:54:46 -0500

    add functions run, chat, and write

--- src/edu/unicauca/control/ControlCustomer.java
+++ src/edu/unicauca/control/ControlCustomer.java
@@ -28,5 +28,51 @@ public class ControlCustomer extends Thread {
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         start();
     }
+    
+    public void run() {
+        String textUser = "";
+        try {
+            out.println("Welcome Java Socket Chat: ");
+            nameCustomer = (in.readLine()).trim();
+            System.out.println("User " + nameCustomer + " connected ");
+            if ((nameCustomer.equals("")) || (nameCustomer == null)) {
+                nameCustomer = "Anonimo";
+            }
+            Iterator it = customerAct.iterator();
+            while (it.hasNext()) {
+                if (nameCustomer.equals(((ControlCustomer) it.next()).nameCustomer)) {
+                    nameCustomer = nameCustomer + socket.getPort();
+                    break;
+                }
+            }
+            AddingChat(this);
+            out.println("name user: " + nameCustomer);
+            while ((textUser = in.readLine()) != null) {
+                writeAll(nameCustomer + " :> " + textUser);
+                GestionArchivoServidor og=new GestionArchivoServidor();
+               // og.guardarChat(textoUsuario, nombreCliente);
+                
+            }
+        } catch (IOException e2) {
+        }
+    }
+    
+     private static synchronized void AddingChat(ControlCustomer control) {
+        customerAct.add(control);
+    }
 
+    private synchronized void writeAll(String textUs) {
+        Iterator it = customerAct.iterator();
+        while (it.hasNext()) {
+            ControlCustomer tmp = (ControlCustomer) it.next();
+            if (!(tmp.equals(this))) {
+                writeCustomer(tmp, textUs);
+            }
+        }
+    }
+
+    private synchronized void writeCustomer(ControlCustomer control, String textUs) {
+        (control.out).println(textUs);
+       
+    }
 }
